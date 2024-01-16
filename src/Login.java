438f209cc295f46bb0e392e0d6d49c445cc24f2d
admin <admin@Vedants-MacBook-Pro.local>
2024-01-16 19:36:07 +0530

    First-Commit

---
+++ src/Login.java
@@ -0,0 +1,70 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Login extends JFrame {
+
+    Login() {
+        super("Login Page");
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+
+        JLabel lblusername = new JLabel("Username");
+        lblusername.setBounds(300,20,100,20);
+        add(lblusername);
+
+        JTextField username = new JTextField();
+        username.setBounds(400,20,150,20);
+        add(username);
+
+        JLabel lblpassword = new JLabel("Password");
+        lblpassword.setBounds(300,60,100,20);
+        add(lblpassword);
+
+        JTextField password = new JTextField();
+        password.setBounds(400,60,150,20);
+        add(password);
+
+        JLabel logginginas = new JLabel("Logging in as");
+        logginginas.setBounds(300,100,100,20);
+        add(logginginas);
+
+        Choice loggingin = new Choice();
+        loggingin.add("Admin");
+        loggingin.add("Customer");
+        loggingin.setBounds(400,100,150,20);
+        add(loggingin);
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/login.png"));
+        Image i2 = i1.getImage().getScaledInstance(16, 16, Image.SCALE_DEFAULT);
+        JButton login = new JButton("Login", new ImageIcon(i2));
+        login.setBounds(330,160,100,20);
+        add(login);
+
+        ImageIcon i3 = new ImageIcon(ClassLoader.getSystemResource("icon/cancel.jpg"));
+        Image i4 = i3.getImage().getScaledInstance(16, 16, Image.SCALE_DEFAULT);
+        JButton cancel = new JButton("Cancel", new ImageIcon(i4));
+        cancel.setBounds(450,160,100,20);
+        add(cancel);
+
+        ImageIcon i5 = new ImageIcon(ClassLoader.getSystemResource("icon/signup.png"));
+        Image i6 = i5.getImage().getScaledInstance(16, 16, Image.SCALE_DEFAULT);
+        JButton signup = new JButton("Sign Up", new ImageIcon(i6));
+        signup.setBounds(380,200,100,20);
+        add(signup);
+
+        ImageIcon i7 = new ImageIcon(ClassLoader.getSystemResource("icon/second.jpg"));
+        Image i8 = i7.getImage().getScaledInstance(250, 250, Image.SCALE_DEFAULT);
+        ImageIcon i9 = new ImageIcon(i8);
+        JLabel image = new JLabel(i9);
+        image.setBounds(0,0,250,250);
+        add(image);
+
+        setSize(640,300);
+        setLocation(400,200);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Login();
+    }
+}
